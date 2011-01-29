ae94891da02a8add5eadbc85a91b8d4198e383f2
Ozan Onay <ozan.onay@gmail.com>
2011-01-28 18:07:57 -0800

    Added production domain. To develop, add a local_settings.py which overwrites WALRUS_DOMAIN

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Project-wide settings for this-is-a-walrus
 import os
 
-WALRUS_DOMAIN = "http://4vps.localtunnel.com"
+WALRUS_DOMAIN = "http://www.spellwalrus.com"
 
 TWILIO_API_VERSION = '2010-04-01'
 
@@ -13,3 +13,9 @@ TWILIO_ACCOUNT_TOKEN = 'e9c6bedc9f1f029d4ff9ec8661e7a9e6'
 TWILIO_CALLER_ID = '(646) 450-2065';
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+
+
+try:
+    from local_settings.py import *
+except ImportError:
+    pass
