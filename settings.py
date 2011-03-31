308085c066413e4feb5cfaddf453f35bb29241f1
Tim Olshansky <tim.olshansky@gmail.com>
2011-03-31 12:50:23 -0700

    Transferred repo from assembla to github

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ WALRUS_DOMAIN = "http://www.spellwalrus.com"
 TWILIO_API_VERSION = '2010-04-01'
 
 # Twilio AccountSid and AuthToken
-TWILIO_ACCOUNT_SID = 'ACaf6b71b908e26093ee7fc9d2562e89d9'
-TWILIO_ACCOUNT_TOKEN = 'e9c6bedc9f1f029d4ff9ec8661e7a9e6'
+TWILIO_ACCOUNT_SID = 'TWILIO_ACCOUNT_SID'
+TWILIO_ACCOUNT_TOKEN = 'TWILIO_ACCOUNT_TOKEN'
 
 # Outgoing Caller ID previously validated with Twilio
-TWILIO_CALLER_ID = '(646) 450-2065';
+TWILIO_CALLER_ID = 'TWILIO_CALLER_ID';
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
